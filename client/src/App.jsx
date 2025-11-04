7fb1d355099e529cdee70c0209525335d96761db
Srihari <76058816+Srihari1176@users.noreply.github.com>
2025-11-04 07:46:54 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -62,7 +62,7 @@ export default function App() {
       </div>
 
       <footer className="mt-8 text-center text-slate-500 text-sm">
-        Built with React, Tailwind, Express, Mongoose.
+        Built with React, Tailwind, Express.
       </footer>
     </div>
   )
